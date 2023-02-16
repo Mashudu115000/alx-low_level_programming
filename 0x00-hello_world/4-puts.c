cc4d55e57c88f1d77f1508e44d775b6dd0e37fc0
Mashudu <thobonic@gmail.com>
2023-02-16 10:13:03 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Eeturn: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
