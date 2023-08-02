573a7e760bf883ae753e6c82b58301a08fd16e0a
Mashudu115000 <thobonic@gmail.com>
2023-08-02 09:07:50 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,16 +2,12 @@
 
 /**
  * _print_rev_recursion - print a string in reverse
- * @s: string to reverse
+ * @s: The string to be printed.
  */
-
 void _print_rev_recursion(char *s)
 {
-        if (*s != '\0')
-        {
-                _print_rev_recursion(s + 1);
-                        _putchar(*s);
-        }
-        else
-                return (0);
+if (*s)
+{
+_print_rev_recursion(s + 1);
+}
 }
