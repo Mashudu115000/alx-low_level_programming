d30c46a3213eac0c40f0c414395fd9314bf9a774
Mashudu115000 <thobonic@gmail.com>
2023-08-08 05:45:31 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Print the result
+ * @argv: number of arguments
+ * @argc: arguments vector 
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, add = 0;
+	
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(arg[i]);
+	}
+	printf("%d\n, add");
+	return (0);
+}
+
